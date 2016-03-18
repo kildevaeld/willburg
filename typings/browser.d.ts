532e4231ae6ca660a9ed0b03f2f826c6e91b0cb5
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2016-03-18 20:00:35 +0100

    v0.1.4

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/formidable/index.d.ts" />
-/// <reference path="browser/ambient/joi/index.d.ts" />
-/// <reference path="browser/ambient/koa/index.d.ts" />
-/// <reference path="browser/ambient/mz/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/formidable/index.d.ts" />
-/// <reference path="main/ambient/joi/index.d.ts" />
-/// <reference path="main/ambient/koa/index.d.ts" />
-/// <reference path="main/ambient/mz/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
