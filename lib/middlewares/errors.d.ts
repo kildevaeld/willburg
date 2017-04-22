fb8bacee38177e15b279b6d388101dda06b62a00
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2017-04-22 18:19:22 +0200

    updated tsconfig to use es6

--- lib/middlewares/errors.d.ts
+++ lib/middlewares/errors.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="node" />
 import { MiddlewareFunc, Context } from '../interfaces';
 export declare function ErrorHandler(fn?: (ctx: Context, error: Error) => any, propagateError?: boolean): MiddlewareFunc;
